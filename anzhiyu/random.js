7ecb062432d27649e5b61cfe323a56ea214508f8
yansend6 <zywtosend@outlook.com>
2025-01-26 10:10:09 +0800

    Site updated: 2025-01-26 10:10:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/26/hello-world/","2025/01/26/新文章/"];function toRandomPost(){
+var posts=["2025/01/26/这是一篇新的博文/","2025/01/26/新文章/","2025/01/26/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
